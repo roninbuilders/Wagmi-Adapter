9be90ef87418098d4606302763c219e21dc38898
Glitch <glitch.txs@gmail.com>
2024-01-30 18:51:21 -0300

    add status

--- playground/next-app/src/components/WalletInfo.tsx
+++ playground/next-app/src/components/WalletInfo.tsx
@@ -3,17 +3,18 @@
 import { useAccount, useDisconnect } from 'wagmi'
 
 function WalletInfo() {
-  const { address } = useAccount()
+  const { address, status } = useAccount()
   const { disconnect } = useDisconnect()
 
   if(address) return (
     <>
       {"user connected as: " + address}
       <button onClick={()=>disconnect()}>Disconnect</button>
+      {status}
     </>
   )
 
-  return null
+  return status
 }
 
 export default WalletInfo
