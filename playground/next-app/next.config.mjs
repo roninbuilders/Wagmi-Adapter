f16530b40b9f0409b06a4b2ee56615ca898009d7
Glitch <glitch.txs@gmail.com>
2024-01-30 16:48:55 -0300

    change out dir for next

--- playground/next-app/next.config.mjs
+++ playground/next-app/next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   webpack: config => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
-  }
+  },
+  distDir: 'dist'
 };
 
 export default nextConfig;
